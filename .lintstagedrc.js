3cc3b76434d97744e8f96e099e1f555220cf3aae
Christian Viana <viana2015christian@gmail.com>
2024-10-13 22:03:00 -0300

    Removido tailwind css

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -3,6 +3,6 @@ module.exports = {
 	'*.{js,jsx,ts,tsx}': (filenames) => [
 		`prettier --write ${filenames.join(' ')}`,
 		`npm run lint --fix . ${filenames.join(' --file')}`,
-		`npm test -- --findRelatedTest ${filenames.join(' ')}`
+		`npm test -- --findRelatedTests ${filenames.join(' ')}`
 	]
 }
